82cb233bfbf78b13ef961615fd447273d5f935e2
vigkast <vigwohlig@gmail.com>
2015-08-26 13:18:41 +0530

    again changes

--- api/controllers/DummyController.js
+++ api/controllers/DummyController.js
@@ -33,15 +33,13 @@ module.exports = {
                         gridStore.open(function (err, gridStore) {
                             gridStore.writeFile(filepath, function (err, doc) {
                                 sails.GridStore.read(db, fileId, function (err, fileData) {
+                                    var buffr = fileData;
                                     res.json(fileId);
                                     sails.fs.unlink(filepath, function (err) {
                                         if (err) {
                                             console.log(err);
                                         }
                                     });
-                                    if (doc) {
-                                        gridStore.close(function () {});
-                                    }
                                 });
                             });
                         });
@@ -68,101 +66,72 @@ module.exports = {
             });
         });
     },
-    resize: function (req, res) {
-        var type = -1;
-        var filetype = '';
+    resizeimage: function (req, res) {
         var file = req.param('file');
         var fd = sails.ObjectID(file);
         var newheight = req.param('height');
         var newwidth = req.param('width');
         if (!newwidth && !newheight) {
-            console.log("if");
             showimage(fd);
         } else if (!newwidth && newheight) {
-            console.log("first elseif");
             newheight = parseInt(newheight);
             findimage(fd, 0, newheight);
         } else if (newwidth && !newheight) {
-            console.log("second elseif");
             newwidth = parseInt(newwidth);
             findimage(fd, newwidth, 0);
         } else {
-            console.log("else");
             findimage(fd, newwidth, newheight);
         }
 
         function findimage(fd, newwidth, newheight) {
-            console.log("my name");
             sails.query(function (err, db) {
                 if (err) {
                     console.log(err);
                 }
-                if (db) {
-                    sails.GridStore.read(db, fd, function (err, fileData) {
-                        var file = new sails.GridStore(db, fd, "r");
-                        file.open(function (err, file) {
-                            if (err) {
-                                console.log(err);
-                            }
-                            filetype = file.contentType;
-                            if (filetype == 'image/gif') {
-                                type = 'gif';
-                            } else if (filetype == 'image/jpeg') {
-                                type = 'jpg';
-                            } else if (filetype == 'image/png') {
-                                type = 'png';
-                            }
-                            uploadimage();
-                        });
-
-                        function uploadimage() {
-                            if (type != -1 && filetype != "") {
-                                width = parseInt(newwidth);
-                                height = parseInt(newheight);
-                                lwip.open(fileData, type, function (err, image) {
-                                    var dimensions = {};
-                                    dimensions.width = image.width();
-                                    dimensions.height = image.height();
-                                    if (width == 0) {
-                                        width = dimensions.width / dimensions.height * height;
-                                    }
-                                    if (height == 0) {
-                                        height = dimensions.height / dimensions.width * width;
-                                    }
+                sails.GridStore.read(db, fd, function (err, fileData) {
+                    width = parseInt(newwidth);
+                    height = parseInt(newheight);
+                    lwip.open(fileData, 'jpg', function (err, image) {
+                        var dimensions = {};
+                        dimensions.width = image.width();
+                        dimensions.height = image.height();
+                        if (width == 0) {
+                            width = dimensions.width / dimensions.height * height;
+                        }
+                        if (height == 0) {
+                            height = dimensions.height / dimensions.width * width;
+                        }
+                        if (err) {
+                            console.log(err);
+                        }
+                        image.resize(width, height, "lanczos", function (err, image2) {
+                            db.open(function (err, db) {
+                                var fileId = new sails.ObjectID();
+                                var mimetype = 'image/jpeg';
+                                var gridStore = new sails.GridStore(db, fileId, 'w', {
+                                    content_type: mimetype
+                                });
+                                gridStore.open(function (err, gridStore) {
                                     if (err) {
                                         console.log(err);
                                     }
-                                    image.resize(width, height, "lanczos", function (err, image2) {
-                                        db.open(function (err, db) {
-                                            var fileId = new sails.ObjectID();
-                                            var mimetype = filetype;
-                                            var gridStore = new sails.GridStore(db, fileId, 'w', {
-                                                content_type: mimetype
-                                            });
-                                            gridStore.open(function (err, gridStore) {
-                                                if (err) {
-                                                    console.log(err);
-                                                }
-                                                image2.toBuffer(type, {}, function (err, imagebuf) {
-                                                    gridStore.write(imagebuf, function (err, doc) {
-                                                        if (err) {
-                                                            console.log(err);
-                                                        }
-                                                        if (doc) {
-                                                            gridStore.close(function () {
-                                                                showimage(fileId);
-                                                            });
-                                                        }
-                                                    });
+                                    image2.toBuffer('jpg', {}, function (err, imagebuf) {
+                                        gridStore.write(imagebuf, function (err, doc) {
+                                            if (err) {
+                                                console.log(err);
+                                            }
+                                            if (doc) {
+                                                gridStore.close(function () {
+                                                    showimage(fileId);
                                                 });
-                                            });
+                                            }
                                         });
                                     });
                                 });
-                            }
-                        }
+                            });
+                        });
                     });
-                }
+                });
             });
         }
 
