6a75d446305388246946ba6d66b000f3db919a9f
vigkast <vigwohlig@gmail.com>
2015-10-13 16:13:49 +0530

    current

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -308,5 +308,9 @@ module.exports = {
     },
     pushWoosh: function(req, res) {
         User.pushWoosh(req.body);
+    },
+    ,
+    currentTime: function(req, res) {
+        res.json(sails.moment().format('DD-MM-YYYY h-mm-ss-SSSSa'));
     }
 };
